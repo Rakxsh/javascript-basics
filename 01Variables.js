8aa465c4d0faec1207d2ec592f840e7fe6a3a284
Rakesh <rakeshasokan06@gmail.com>
2021-06-26 13:06:46 +0530

    first commit

---
+++ 01Variables.js
@@ -0,0 +1,14 @@
+var fullName = "rakesh";
+
+var courseName = "Js"
+
+var isLoggedIn = true;
+
+var loggedCount = 34;
+
+console.log(loggedCount);
+
+var paymentMode;
+
+paymentMode = "Credit card"
+
